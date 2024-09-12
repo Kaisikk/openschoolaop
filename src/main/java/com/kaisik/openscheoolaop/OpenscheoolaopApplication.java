29d711aae5af7bd1322c170bea827f726d2e11eb
Kaisikk <Kosikov.Ilya@mail.ru>
2024-09-12 23:21:02 +0400

    Создание структуры проекта

---
+++ src/main/java/com/kaisik/openscheoolaop/OpenscheoolaopApplication.java
@@ -0,0 +1,13 @@
+package com.kaisik.openscheoolaop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenscheoolaopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenscheoolaopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kaisik/openscheoolaop/OpenscheoolaopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaisik.openscheoolaop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenscheoolaopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
